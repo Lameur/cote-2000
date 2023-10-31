b84d7e881f1d0b36c93539774a9c905f1dc462e2
Lameur <96985489+Lameur@users.noreply.github.com>
2023-10-31 12:47:22 +0100

    Update

---
+++ index(var).js
@@ -0,0 +1,2 @@
+var OEConfWECounter = {"WE84a777c119":{"ScriptVersion":22,"Config":{"TypeCounter":0,"InitialValue":0,"Lenght":4,"CountMethod":1}}}
+var OEConfWEDisableRightClick = {"WEfcee4be359":{"Text":{"Items":{"DEFAULT":"Clic droit interdit"}}}}
---
+++ index(var).php
@@ -0,0 +1,3 @@
+<?php
+$OESiteUploadDirectory = "Upload";
+$OEConfWECounter = '{"WE84a777c119":{"ScriptVersion":22,"Config":{"TypeCounter":0,"InitialValue":0,"Lenght":4,"CountMethod":1}}}';
